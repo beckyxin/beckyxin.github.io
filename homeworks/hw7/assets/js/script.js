6f70e3fd2772e7522b4c2611699a2311f930bed2
beckyxin <113162321+beckyxin@users.noreply.github.com>
2022-11-01 20:48:48 -0700

    finish hw7

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,23 @@
+function averageThreeNumbers(a,b,c){
+    let sum = a + b + c;
+    let average = sum/3;
+    return average;
+}
+
+function createSentence(num, noun){
+    return "On average, a Berkeley student has " + num +" " + noun +"s";
+}
+
+function getRandomNum(max){
+    return Math.floor(Math.random() * max);
+}
+
+x = getRandomNum(20);
+y = getRandomNum(10);
+z = getRandomNum(13);
+
+avg = averageThreeNumbers(x,y,z);
+
+sentence = createSentence(avg, "Kitten");
+
+console.log(sentence);
